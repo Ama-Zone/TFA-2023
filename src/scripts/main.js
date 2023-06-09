9fbf9cfeddb75f99e7665658420e349631a5efa3
Ama-Zone <100293017+Ama-Zone@users.noreply.github.com>
2023-06-09 16:30:23 +0200

    slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,51 @@
-console.info('Hello world');
+"use strict";
+const btnPrev = document.querySelector(".slider__btn--prev"),
+      btnnext = document.querySelector(".slider__btn--next");
+
+      btnnext.addEventListener("click", next);
+      btnPrev.addEventListener("click", prev);
+      
+      //touche clavier
+
+      document.addEventListener("keydown" ,function(e){
+          if (e.code == "ArrowLeft"){
+              prev();
+          }else if (e.code == "ArrowRight"){
+            next();
+          }
+      }); 
+
+      //suite slider
+
+      function next(){
+
+        let elShow = document.querySelector(".slider__el--show"),
+            elnext = elShow.nextElementSibling;
+
+            elShow.classList.remove("slider__el--show");
+
+            if(elnext){
+                elnext.classList.add("slider__el--show");
+            }else{
+                let elfirst = elShow.parentNode.firstElementChild;
+                elfirst.classList.add("slider__el--show");
+            }
+
+            
+            
+      }
+
+      function prev(){
+
+        let elShow = document.querySelector(".slider__el--show"),
+            elPrev = elShow.previousElementSibling;
+
+            elShow.classList.remove("slider__el--show");
+
+            if(elPrev){
+                elPrev.classList.add("slider__el--show");
+            }else{
+                let elLast = elShow.parentNode.lastElementChild;
+                elLast.classList.add("slider__el--show");
+            }
+        }
